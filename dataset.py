f1cdbdb7aa0e89166facfa715216174fbfc22994
Monish <monish.ramadoss@outlook.com>
2021-10-09 01:53:17 -0400

    update

--- dataset.py
+++ dataset.py
@@ -12,7 +12,7 @@ class nih_dataset_classification(Sequence):
     def __init__(self, dir='~/data/nih'):
         self.df = pd.read_csv(join(dir, 'Data_Entry_2017.csv'))
         self.data_files = self.df[['Image Index', 'Finding Labels']]
-        self.files = glob.glob(dir + '/**/*.png', "**", recursive=True, )
+        self.files = glob.glob(dir + '/**/*.png', recursive=True, )
         print(len(self.files))
     def __len__(self,):
         pass

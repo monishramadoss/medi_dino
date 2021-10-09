10f7c0302c2045f410fe720a8ee6519ba74614a0
Monish <monish.ramadoss@outlook.com>
2021-10-09 01:40:40 -0400

    update

---
+++ dataset.py
@@ -0,0 +1,21 @@
+import os
+import glob
+import tensorflow as tf
+import tensorflow.keras as K
+from tensorflow.keras.utils import Sequence
+import pandas as pd
+
+join = os.path.join
+
+
+class nih_dataset_classification(Sequence):
+    def __init__(self, dir='~/data/nih'):
+        self.df = pd.read_csv(join(dir, 'Data_Entry_2017.csv'))
+        self.data_files = self.df['Image Index', 'Finding Labels']
+        self.files = glob.glob('*.png', "**", recursive=True, root_dir=dir)
+        print(len(self.files))
+    def __len__(self,):
+        pass
+
+    def __getitem__(self, idx):
+        pass
---
+++ train.py
@@ -0,0 +1,17 @@
+from model import VIT, Vit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass

e4c6a0b23c005e5a05244cd88c1900631adc163e
monishramadoss <monish.ramadoss@outlook.com>
2021-10-27 15:20:02 -0700

    dataloader update

--- dataset.py
+++ dataset.py
@@ -6,49 +6,72 @@ import tensorflow.keras as K
 from tensorflow.keras.utils import Sequence
 import pandas as pd
 from skimage.io import imread
-
+import numpy as np
 
 join = os.path.join
 
+def label_split(label, label_dict={}):
+    one_hot = [0 for _ in range(len(label_dict))]
+    if '|' not in label:
+        if label not in label_dict:
+            label_dict[label] = len(label_dict)
+            one_hot.append(1)
+        else:
+            one_hot[label_dict[label]] = 1
+    else:
+        for l in label.split('|'):
+            if l not in label_dict:
+                label_dict[l] = len(label_dict)
+                one_hot.append(1)
+            else:
+                one_hot[label_dict[l]] = 1
+    return one_hot
 
 class nih_dataset(Sequence):
     def __init__(self, dir='/home/mramados/data/nih', test=False, bounding_box=False, batch_size=8):
+        print("Loading CSV")
         self.batch_size = batch_size
         self.bounding_box = bounding_box
         if bounding_box:
             self.df = pd.read_csv(join(dir, 'BBox_List_2017.csv'))
-            self.data_files = self.df[['Image Index', 'Finding Labels']]            
+            self.data_files = self.df
         else:
             self.df = pd.read_csv(join(dir, 'Data_Entry_2017.csv'))
-            self.data_files = self.df
+            self.data_files = self.df[['Image Index', 'Finding Labels']]            
         
-        self.classification_label = dict([(v, i) for v, i in enumerate(pd.unique(self.data_files['Finding Labels']))])
+        labels = pd.unique(self.data_files['Finding Labels'])
+        self.label_dict = {}
+        for label in labels:
+            label_split(label, self.label_dict)
         
         if test:
             self.image_names = open(join(dir, 'test_list.txt')).readlines()
         else:
             self.image_names = open(join(dir, 'train_val_list.txt')).readlines()
-       
-        self.files = dict([(os.path.basename(f), f) for f in glob.iglob(join(dir, '/**/*.png'), recursive=True)])
-        print(len(self.files))
-        
+        print("Loading Files Paths")
+        self.files = dict()
+        for file_path in glob.iglob(join(dir, "**/*.png"), recursive=True):
+            if file_path.lower().endswith(".png"):
+                self.files[os.path.basename(file_path)] = file_path
+                
     def __len__(self,):
         return math.ceil(len(self.image_names)  / self.batch_size)
 
     def __getitem__(self, idx):
-        image_names = self.image_names[idx]
-        images = []
-        labels = []
-        
-        for image_path in image_names:
-            image = imread(self.files[image_name])
-            im_frame = self.data_files.loc[self.data_files['Image Index'] == image_name]
-            if not self.bounding_box:
-                label = classification_label[im_fram['Finding Labels']]
-                labels.append(label)
-            images.append(image)
-        
-        return np.array(image), np.array(labels)
+        image_name = self.image_names[idx]
+        image_name = image_name.strip()
+        image = imread(self.files[image_name])
+        label = []
+        if len(image.shape) != 2:
+            print(image_name)
+        im_frame = self.data_files.loc[self.data_files['Image Index'] == image_name]
+        if not self.bounding_box:
+            label_str = im_frame['Finding Labels'].to_list()
+            label = label_split(label_str[0], self.label_dict)
+            
+        images = np.array(image)
+        labels = np.array(label)
+        return images, labels
 
 if __name__ == "__main__":
     dataset = nih_dataset()
